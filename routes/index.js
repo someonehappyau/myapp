ed237b8fbcd73fc0664da113b55f52d20c68bb9a
someonehappy <someonehappyau@gmail.com>
2015-09-16 00:50:55 +1000

    playing myqws

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var request=require('request');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -7,7 +8,14 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/info',function(req,res){
-	res.render('info',{pam1:111,pam2:'abcdefg',pamlist:['aa','bb','cc'], pamobj:{a:1,b:'2afdaf'}});
+	request.get('http://localhost:3011/mc/model/cbr600',function(err,response,body){
+		if (!err && response.statusCode==200){
+			console.log(body);
+			console.log(JSON.parse(body));
+			res.render('info',{pamret:JSON.parse(body),pam1:111,pam2:'abcdefg',pamlist:['aa','bb','cc'], pamobj:{a:1,b:'2afdaf'}});
+		}
+	});
+		
 });
 
 module.exports = router;
